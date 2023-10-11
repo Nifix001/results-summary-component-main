a0114a0333fdb039c589bd2dc2f084edafa70750
Nifix001 <101341614+Nifix001@users.noreply.github.com>
2023-10-11 22:21:31 +0100

    update

--- Result Summary/src/App.jsx
+++ Result Summary/src/App.jsx
@@ -34,19 +34,19 @@ const summary = data.map(x =>{
       <div className="lg:max-2xl:flex lg:max-2xl:w-1/2 lg:max-2xl:h-3/5 xs:max-md:w-screen xs:max-md:h-full xs:max-md:grid " >
         <div className=" bg-gradient-to-b from-primary to-primary1 lg:max-2xl:h-full lg:max-2xl:w-1/2 lg:max-2xl:rounded-2xl xs:max-md:h-full xs:max-md:-mt-8 text-center text-white font-hanken xs:max-md:w-full xs:max-md:rounded-b-3xl xs:max-md:pb-12 flex flex-col justify-center items-center">
           <header>
-          <h3 className=" text-gray-300 text-xl font-medium mt-12 ">Your Result</h3>
+          <h3 className=" text-gray-300 text-xl font-medium mt-16 ">Your Result</h3>
           </header>
           <div className="bg-gradient-to-b from-circle to-circle2 rounded-full w-1/3 xs:max-md:h-auto xs:max-md:w-1/3 h-fit grid items-center justify-center gap-0 m-auto p-6 mt-6">
-            <span className=" text-6xl font-extrabold">{total}</span><br /><span className="font-medium text-gray-300 text-sm">of 100</span>
+            <span className="font-extrabold text-7xl">{total}</span><br /><span className="font-medium text-gray-300 text-sm">of 100</span>
           </div>
           <h3 className="text-2xl mb-4 font-medium">Great</h3>
           <p className="mb-6 font-medium text-gray-300 text-lg">You scored higher than 65% of<br /> people who have taken<br /> these tests</p>
         </div>
           <div className="lg:max-2xl:h-full lg:max-2xl:w-1/2 lg:max-2xl:rounded-x-2xl shadow-2xl py-4 px-4">
-            <h3 className="text-left text-xl font-medium my-8 ml-4 xs:max-md:-mt-8 xs:max-md:mb-0">Summary</h3> 
+            <h3 className="text-left text-xl font-medium my-8 ml-4 xs:max-md:-mt-8 mb-4">Summary</h3> 
             {summary}
             <div className="flex items-center justify-center">
-            <button className="bg-sec hover:bg-visual px-28 py-3 text-white rounded-3xl mt-8">Continue</button>
+            <button className="bg-sec hover:bg-visual px-28 py-3 text-white rounded-3xl mt-6">Continue</button>
             </div>
           </div>  
       </div>
